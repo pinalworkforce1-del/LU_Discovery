dd172827e95dc554e2373d17639399af23dcff86
pinalworkforce1-del <pinal.workforce1@gmail.com>
2026-09-07 20:21:30 -0700

    Fix Discovery UX refresh in active src build
    * Add active Discovery UX v1.2 stylesheet
    
    * Add active Discovery scene-control rail
    
    * Wire Discovery UX v1.2 into active src entry

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { DiscoveryExperience } from "@/components/discovery-experience";
 import "./index.css";
+import "./level-up-standard.css";
+import "./level-up-standard";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
